a9db63efc96dfebd896229138c497f4fe2d78ad0
philipkavanagh.com <philkav1989@gmail.com>
2017-01-22 09:49:19 +0000

    Added testing function and changed variable names

--- html_strip.py
+++ html_strip.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python
 
 def strip_html(line):
-	mode = 1
+	quote = False
+	tag = False
 	out = ""
 	for c in line:
-		if c == '<':
-			mode = 0
-		elif c == '>':
-			mode = 1
-		elif mode == 1:
+		if c == '<' and not quote:
+			tag = True
+		elif c == '>' and not quote:
+			tag = False
+		elif c == '"' or c == "'" and tag:
+			quote = not quote ## invert the value
+		elif not tag:
 			out = out + c
 
 	return out
 
-print strip_html("<b>Hello</b> Philip!")
-print strip_html('<b>Hello</b> <a href="www.google.com">Philip!</a>')
+def tests():
+	print strip_html("<b>Hello</b> Philip!")
+	print strip_html('<b>Hello</b> <a href="www.google.com">Philip!</a>')
+	print strip_html('<a href=">">Hi "Philly"</a>')
+
+	print strip_html('<b>bold text<b>')
+	print strip_html('<b>"bold text"<b>')
+	print strip_html('"<b>bold text<b>"')
+	print strip_html('<"b">bold text<"b">')
+
+tests()
